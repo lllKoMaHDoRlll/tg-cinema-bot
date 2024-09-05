ea195b3e71869028651133f38dc844e1904af991
Koma <aleksandr.orlichenya@gmail.com>
2024-09-05 19:39:10 +0300

    create db connection instance

---
+++ src/db/repositories/film.repository.ts
@@ -0,0 +1,38 @@
+import connection from "../";
+import { Movie } from "../../types/movies/movies";
+import Film from "../models/film.model";
+
+interface IFilmRepository {
+    create(film: Movie): Promise<void>;
+    getByInternalId(internalId: number): Promise<Film | undefined>;
+}
+
+class FilmRepository implements IFilmRepository {
+    create(film: Movie): Promise<void> {
+        return new Promise((resolve, reject) => {
+            connection.query(
+                "INSERT INTO films (name, alternative_name, external_id, internal_id, year, description, short_description, poster) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                [film.name, film.alternativeName, film.externalId, film.internalId, film.year, film.description, film.shortDescription, film.poster],
+                (err, res) => {
+                    if (err) reject(err);
+                    else resolve();
+                }
+            );
+        });
+    }
+
+    getByInternalId(internalId: number): Promise<Film | undefined> {
+        return new Promise((resolve, reject) => {
+            connection.query<Film[]>(
+                "SELECT * FROM films WHERE internal_id = ?",
+                [internalId],
+                (err, res) => {
+                    if (err) reject(err);
+                    else resolve(res?.[0]);
+                }
+            );
+        });
+    }
+}
+
+export default new FilmRepository();
