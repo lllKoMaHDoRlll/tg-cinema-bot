7ec3248c5884191bf5538d60222148434defe59e
Koma <aleksandr.orlichenya@gmail.com>
2024-09-05 16:10:31 +0300

    add Movie type

---
+++ src/types/movies/movies.ts
@@ -0,0 +1,10 @@
+
+export type Movie = {
+    name: string
+    alternativeName: string
+    id: number
+    year: number
+    description: string
+    shortDescription: string
+    poster: string
+}
